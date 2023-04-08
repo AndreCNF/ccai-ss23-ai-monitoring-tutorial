464e4001df6c2a9466fb8a699773954084e03ba0
André Ferreira <andrecnf@gmail.com>
2023-04-08 13:28:08 +0100

    Clean emissions data + merge everything together

--- src/coal_emissions_monitoring/data_cleaning.py
+++ src/coal_emissions_monitoring/data_cleaning.py
@@ -3,8 +3,10 @@ from typing import Callable, Optional, Union
 
 import pandas as pd
 import geopandas as gpd
+import overpy
 
 GLOBAL_CRS = "EPSG:4326"
+OSM_API = overpy.Overpass()
 
 
 def clean_column_names(
@@ -33,13 +35,38 @@ def clean_column_names(
     return df
 
 
+def load_clean_data_df(
+    data_path: Union[str, Path],
+    load_func: Optional[Callable] = pd.read_csv,
+    clean_func: Optional[Callable] = clean_column_names,
+) -> pd.DataFrame:
+    """
+    Load and clean a data frame.
+
+    Args:
+        data_path (Union[str, Path]):
+            Path to data
+        load_func (Optional[Callable]):
+            Function to load data
+        clean_func (Optional[Callable]):
+            Function to clean data
+
+    Returns:
+        df (pd.DataFrame):
+            Cleaned data frame
+    """
+    df = load_func(data_path)
+    df = clean_func(df)
+    return df
+
+
 def load_clean_data_gdf(
     data_path: Union[str, Path],
     load_func: Optional[Callable] = pd.read_csv,
     clean_func: Optional[Callable] = clean_column_names,
 ) -> gpd.GeoDataFrame:
     """
-    Load and clean a data frame.
+    Load and clean a data frame, outputting it as a GeoDataFrame.
 
     Args:
         data_path (Union[str, Path]):
@@ -53,8 +80,9 @@ def load_clean_data_gdf(
         gdf (gpd.GeoDataFrame):
             Cleaned data frame
     """
-    df = load_func(data_path)
-    df = clean_func(df)
+    df = load_clean_data_df(
+        data_path=data_path, load_func=load_func, clean_func=clean_func
+    )
     gdf = gpd.GeoDataFrame(
         df,
         geometry=gpd.points_from_xy(
@@ -153,15 +181,19 @@ def clean_campd_facilities(df: pd.DataFrame) -> pd.DataFrame:
     df = df.reset_index()[
         ["facility_id", "facility_name", "year", "capacity_mw", "latitude", "longitude"]
     ]
+    # fix datetime column data type
+    df.year = pd.to_datetime(df.year, format="%Y")
     return df
 
 
-def load_clean_campd_facilities_gdf(campd_path: Union[str, Path]) -> gpd.GeoDataFrame:
+def load_clean_campd_facilities_gdf(
+    campd_facilities_path: Union[str, Path]
+) -> gpd.GeoDataFrame:
     """
     Load and clean the CAMPD facilities data frame.
 
     Args:
-        campd_path (Union[str, Path]):
+        campd_facilities_path (Union[str, Path]):
             Path to CAMPD facilities data
 
     Returns:
@@ -169,5 +201,97 @@ def load_clean_campd_facilities_gdf(campd_path: Union[str, Path]) -> gpd.GeoData
             Cleaned CAMPD facilities data frame
     """
     return load_clean_data_gdf(
-        data_path=campd_path, load_func=pd.read_csv, clean_func=clean_campd_facilities
+        data_path=campd_facilities_path,
+        load_func=pd.read_csv,
+        clean_func=clean_campd_facilities,
     )
+
+
+def clean_campd_emissions(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean the CAMPD emissions data frame.
+
+    Args:
+        df (pd.DataFrame):
+            CAMPD emissions data frame
+
+    Returns:
+        df (pd.DataFrame):
+            Cleaned CAMPD emissions data frame
+    """
+    df = clean_column_names(df)
+    # fix datetime column data type
+    df.date = pd.to_datetime(df.date)
+    return df
+
+
+def load_clean_campd_emissions_df(
+    campd_emissions_path: Union[str, Path]
+) -> pd.DataFrame:
+    """
+    Load and clean the CAMPD emissions data frame.
+
+    Args:
+        campd_emissions_path (Union[str, Path]):
+            Path to CAMPD emissions data
+
+    Returns:
+        df (pd.DataFrame):
+            Cleaned CAMPD emissions data frame
+    """
+    return load_clean_data_df(
+        data_path=campd_emissions_path,
+        load_func=pd.read_csv,
+        clean_func=clean_campd_emissions,
+    )
+
+
+def load_osm_data(
+    country: str = "United States", tag: str = "man_made", value: str = "cooling_tower"
+) -> gpd.GeoDataFrame:
+    """
+    Load OSM data.
+
+    Args:
+        country (str):
+            Country to filter to
+        tag (str):
+            OSM tag to filter to
+        value (str):
+            OSM value to filter to
+
+    Returns:
+        gdf (gpd.GeoDataFrame):
+            OSM cooling towers data frame
+    """
+    # load the data
+    osm_results = OSM_API.query(
+        query=f"""
+        area[name="{country}"]->.searchArea;
+        (
+        node["{tag}"="{value}"](area.searchArea);
+        way["{tag}"="{value}"](area.searchArea);
+        relation["{tag}"="{value}"](area.searchArea);
+        );
+        out body;
+        >;
+        out skel qt;
+        """
+    )
+    df = pd.DataFrame(
+        [
+            {
+                "osm_id": element.id,
+                "latitude": element.lat,
+                "longitude": element.lon,
+            }
+            for element in osm_results.nodes
+        ]
+    )
+    # convert to geodataframe
+    gdf = gpd.GeoDataFrame(
+        df,
+        geometry=gpd.points_from_xy(df.longitude, df.latitude),
+        crs="EPSG:4326",
+    )
+    return gdf
